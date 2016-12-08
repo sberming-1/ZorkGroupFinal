b346539018e5fb432a7a573e18914ef7297862fb
Sean Bermingham <seanmb2015@gmail.com>
2016-12-07 23:27:58 -0500

    Add files via upload

--- ScoreCommand.java
+++ ScoreCommand.java
@@ -23,6 +23,13 @@ public class ScoreCommand extends Command
     * @return - String
     */
    public String execute(){
-       return Integer.toString(GameState.instance().getCurrentScore())+"\n";
+       int i = GameState.instance().getCurrentScore();
+       String rank;
+       if(i > 90){rank = "You are the greatest wizard of all time";}
+       else if(90>=i && i>75){rank = "You could be a professor";}
+       else if(75>=i && i> 50){rank = "You are at the top of your class";}
+       else if(50>=i && i>25){rank = "Keep doing well";}
+       else{rank = "You are just getting started";}
+       return Integer.toString(i) + rank +"\n";
    }
 }
